68b4a41d66035e6f776a5d0caba1e8ef4bb48b63
Hoang M Tran <tran189@purdue.edu>
2019-09-11 16:27:43 -0400

    Done!

--- answers.sh
+++ answers.sh
@@ -4,37 +4,37 @@
 cd ORIGIN
 
 # Task 1 
-
+mv  Homework/MA165/HW2.txt Homework/MA165/HW1.txt
 
 # Task 2
-
+rm Memes/SpongeBob/money.jpeg
 
 # Task 3
-
+mv Memes/Star\ Wars/fafsa.jpg Memes/SpongeBob
 
 # Task 4
-
+rm Homework/CS191 -r
 
 # Task 5
-
+mkdir Programs/C++
 
 # Task 6
-
+mv Programs/Age.cpp Programs/C++
 
 # Task 7
-
+mv Programs/Java Homework/CS180/Project_1
 
 # Task 8
-
+mv Programs/*.c Programs/C
 
 # Task 9
-
+mv Programs/*.py Programs/Python\ 3
 
 # Task 10
-
+touch Homework/POL141/Essay.txt
 
 # Task 11
-
+ls ..
 
 # Task 12
-
+alias wttr="curl wttr.in"
